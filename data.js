332de80b0c5efce90a2a4a63e3114ffeaf31aa20
David McCullough <dmccullough488@gmail.com>
2025-08-17 19:40:15 -0500

    added header & nav bar with styling

---
+++ data.js
@@ -0,0 +1,27 @@
+
+const posts = [
+  {
+    id: 1,
+    title: "My new journey as a bootcamp student.",
+    date: "July 23, 2022",
+    content: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects and meet like-minded peers.",
+    image: "images/bootcamp.jpg",
+    featured: true
+  },
+  {
+    id: 2,
+    title: "Blog one",
+    date: "July 23, 2022",
+    content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    image: "images/blog1.jpg",
+    featured: false
+  },
+  {
+    id: 3,
+    title: "Blog two",
+    date: "July 23, 2022",
+    content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    image: "images/blog2.jpg",
+    featured: false
+  },
+];
